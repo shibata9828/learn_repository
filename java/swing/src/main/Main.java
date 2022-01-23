5b5dc9bb31830258990e6a6dfd7707f805652804
shibata9828 <m7bp67nkma@gmail.com>
2022-01-23 16:08:08 +0900

    start swing praictice

---
+++ java/swing/src/main/Main.java
@@ -0,0 +1,14 @@
+package src.main;
+
+public class Main {
+  static MainWindow mainWindow;
+
+  public static void main(String[] args) {
+    mainWindow = new MainWindow();
+    mainWindow.preparePanels();
+    mainWindow.prepareComponents();
+    mainWindow.setFontScreenAndFocus(ScreenMode.TITLE);
+    mainWindow.setVisible(true);
+
+  }
+}
